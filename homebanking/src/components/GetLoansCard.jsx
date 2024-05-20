5841f3647b36200f449fae68244a7bab6755fe0b
Agustin-del <eddyruveda@gmail.com>
2024-05-19 23:25:44 -0300

    center cards titles

--- homebanking/src/components/GetLoansCard.jsx
+++ homebanking/src/components/GetLoansCard.jsx
@@ -3,22 +3,22 @@ import { Card } from "flowbite-react";
 export function GetLoansCard({name, amount, payments}) {
   return (
     <Card className="max-w-sm">
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <h5 className="text-2xl text-center font-bold tracking-tight text-gray-900">
         {name}
       </h5>
       <div className= "flex gap-2 items-center">
-        <p className="font-normal text-gray-700 dark:text-gray-400">
+        <p className="font-normal text-gray-700">
           Amount:
         </p>
-        <p className="font-normal text-gray-700 text-2xl dark:text-gray-400">
+        <p className="font-normal text-gray-700 text-2xl">
           ${amount}
         </p>
       </div>
       <div className= "flex gap-2 items-center">
-        <p className="font-normal text-gray-700 dark:text-gray-400">
+        <p className="font-normal text-gray-700">
           Payments:
         </p>
-        <p className="font-normal text-gray-700 text-2xl dark:text-gray-400">
+        <p className="font-normal text-gray-700 text-2xl">
           {payments}
         </p>
       </div>
