137463498c8d9ad71d0f56dd50fd81250b255812
Agustin-del <eddyruveda@gmail.com>
2024-06-19 16:13:54 -0300

    f

--- homebanking/src/pages/LandingAuthenticated.jsx
+++ homebanking/src/pages/LandingAuthenticated.jsx
@@ -73,29 +73,27 @@ export const LandingAuthenticated = () => {
     }
   }
   return (
-        <div className="flex flex-col justify-evenly flex-1 w-full p-4 gap-4">
-          {loading ? 
-          <Box minWidth={300} minHeight={300}>
+        loading ? 
+          <Box minwidth={"100%"} minHeight={"100%"}>
             <Skeleton variant ="text"  width={210} height={40}/> 
             <Skeleton variant="rectangular"  width={210} height={118} /> 
             <Skeleton variant="text" width="60%" />
           </Box>
           :
           <>
-            <Welcome client={clientData.firstName + ' ' + clientData.lastName}/>
-            <div className="flex flex-wrap justify-evenly gap-4">
-              {accountData && accountData.map(account => {
-                return <GetAccountCard accountId ={account.id} key={account.id} number={account.number} balance={account.balance}/>
-              })}
-            </div>
-            <div className="flex justify-center">
-              {creating && <Alert color="info">Creating your account...</Alert>}
-              {error && <Alert color="failure">{error}</Alert>}
+            <div className="flex flex-col justify-evenly flex-1 w-full p-4 gap-4"></div>
+              <Welcome client={clientData.firstName + ' ' + clientData.lastName}/>
+              <div className="flex flex-wrap justify-evenly gap-4">
+                {accountData && accountData.map(account => {
+                  return <GetAccountCard accountId ={account.id} key={account.id} number={account.number} balance={account.balance}/>
+                })}
+              </div>
+              <div className="flex justify-center">
+                {creating && <Alert color="info">Creating your account...</Alert>}
+                {error && <Alert color="failure">{error}</Alert>}
             </div>
               {isDesktop ? <Button className="self-center" onClick={handleNewAccount}>Request new account</Button> : <Button className="self-center" onClick={handleNewAccount}><p className="text-xs">Request new account</p></Button>}
-              <Carrousel/>
+            <Carrousel/>
           </>
-          }
-        </div>
-  )
-}
+          ) 
+        }
