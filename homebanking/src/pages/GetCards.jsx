d5c34c9da656a3592c8b191e15185e1624a5f35d
Agustin-del <eddyruveda@gmail.com>
2024-06-19 14:52:57 -0300

    f

--- homebanking/src/pages/GetCards.jsx
+++ homebanking/src/pages/GetCards.jsx
@@ -40,7 +40,7 @@ export const GetCards = () => {
   return (
     <>
     {loading ? 
-      <Box height={full} width={full} >
+      <Box height={300} width={1000} >
         <Skeleton variant ="text"  width={"80%"} height={40}/> 
         <Skeleton variant ="text"  width={"80%"} height={40}/> 
         <Skeleton variant="rectangular"  width={210} height={118} /> 
